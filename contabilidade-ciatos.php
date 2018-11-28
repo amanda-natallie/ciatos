9bd2836ecd1611370fe09e801b1ad0c6900ef29e
Amanda <Amanda@Blumenkranz-pc>
2018-11-27 23:56:50 -0200

    Mais umas seções aqui e ali

--- contabilidade-ciatos.php
+++ contabilidade-ciatos.php
@@ -13,7 +13,122 @@
             </div>
         </div>
     </div>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="tabbable-panel ptb-servicos">
+                    <div class="tabbable-line">
+                        <ul class="nav nav-tabs text-center">
+                            <li class="active">
+                                <a href="#tab1" data-toggle="tab">
+                                    <img src="assets/img/cont-services/1.png" alt="" title=""><br>
+                                    Contabilidade <br>Geral
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#tab2" data-toggle="tab">
+                                    <img src="assets/img/cont-services/2.png" alt="" title=""><br>
+                                   <br>Societário
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#tab3" data-toggle="tab">
+                                    <img src="assets/img/cont-services/3.png" alt="" title=""><br>
+                                    Gestão de <br> Pessoas
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#tab4" data-toggle="tab">
+                                    <img src="assets/img/cont-services/4.png" alt="" title=""><br>
+                                    Gestão  <br> Financeira
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#tab5" data-toggle="tab">
+                                    <img src="assets/img/cont-services/5.png" alt="" title=""><br>
+                                    Acessoria  <br> Tributária
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#tab6" data-toggle="tab">
+                                    <img src="assets/img/cont-services/6.png" alt="" title=""><br>
+                                    SPED, SINTEGRA  <br> E-SOCIAL
+                                </a>
+                            </li>
+                            
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="tab1" style="background: url(assets/img/cont-services/bg/1.png) top right no-repeat; background-size: 60%">
+                                <div class="space-50"></div>
+                                    <h4>Contabilidade Geral</h4>
+                                    <ul>
+                                        <li>Contábil</li>
+                                        <li>Fiscal</li>
+                                        <li>Departamento Pessoal</li>
+                                        <li>Paralegal</li>
+                                    </ul>
+                                
+                               
+                            </div>
+                            <div class="tab-pane" id="tab2" style="background: url(assets/img/cont-services/bg/2.png) top right no-repeat; background-size: 60%">
+                                <div class="space-50"></div>
+                                    <h4>Societário</h4>
+                                    <ul>
+                                        <li>Contábil</li>
+                                        <li>Fiscal</li>
+                                        <li>Departamento Pessoal</li>
+                                        <li>Paralegal</li>
+                                    </ul>
+                            </div>
+                            <div class="tab-pane" id="tab3" style="background: url(assets/img/cont-services/bg/3.png) top right no-repeat; background-size: 60%">
+                               <div class="space-50"></div>
+                                    <h4>Gestão de Pessoas</h4>
+                                    <ul>
+                                        <li>Contábil</li>
+                                        <li>Fiscal</li>
+                                        <li>Departamento Pessoal</li>
+                                        <li>Paralegal</li>
+                                    </ul>
+                            </div>
+                            <div class="tab-pane" id="tab4" style="background: url(assets/img/cont-services/bg/4.png) top right no-repeat; background-size: 60%">
+                               <div class="space-50"></div>
+                                    <h4>Gestão Financeira</h4>
+                                    <ul>
+                                        <li>Contábil</li>
+                                        <li>Fiscal</li>
+                                        <li>Departamento Pessoal</li>
+                                        <li>Paralegal</li>
+                                    </ul>
+                            </div>
+                            <div class="tab-pane" id="tab5" style="background: url(assets/img/cont-services/bg/5.png) top right no-repeat; background-size: 60%">
+                               <div class="space-50"></div>
+                                    <h4>Acessoria Tributária</h4>
+                                    <ul>
+                                        <li>Contábil</li>
+                                        <li>Fiscal</li>
+                                        <li>Departamento Pessoal</li>
+                                        <li>Paralegal</li>
+                                    </ul>
+                            </div>
+                            <div class="tab-pane" id="tab6" style="background: url(assets/img/cont-services/bg/1.png) top right no-repeat; background-size: 60%">
+                               <div class="space-50"></div>
+                                    <h4>SPED, SINTEGRA  E-SOCIAL</h4>
+                                    <ul>
+                                        <li>Contábil</li>
+                                        <li>Fiscal</li>
+                                        <li>Departamento Pessoal</li>
+                                        <li>Paralegal</li>
+                                    </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
     <?php require 'footer.php'; ?>    
 </div>
 
--- index.php
+++ index.php
@@ -1,55 +1,197 @@
 <?php require 'header.php'; ?>
-        <a href="#" class="flotada flot-um hvr-forward hidden-xs">
-            <h4>SOU CLIENTE CIATOS!</h4>
-        </a>
-        <a  href="" class="flotada flot-dois hvr-forward hidden-xs">
-            <h6>CONTATO</h6>
-        </a>
-        <div class="banner_home" style="background: url(assets/img/bg_banner.png)">
+<a href="#" class="flotada flot-um hvr-forward hidden-xs">
+    <h4>SOU CLIENTE CIATOS!</h4>
+</a>
+<a  href="" class="flotada flot-dois hvr-forward hidden-xs">
+    <h6>CONTATO</h6>
+</a>
+<div class="banner_home" style="background: url(assets/img/bg_banner.png)">
 
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 text-center logo">
-                        <a href="#" class="hvr-grow"><img src="assets/img/logo.png" alt="" title=""></a>
-                    </div>
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center logo">
+                <a href="#" class="hvr-grow"><img src="assets/img/logo.png" alt="" title=""></a>
             </div>
-            <div class="container">
-                <div class="row row-banner">
-                    <div class="col-lg-7 col-lg-offset-1 col-md-5 col-md-offset-2 col-sm-9 col-sm-offset-3 col-xs-12">
-                        <h3 class="principal-title">
-                            O GRUPO CIATOS PRESTA TODOS OS SERVIÇOS CONTÁBEIS QUE SUA EMPRESA POSSA PRECISAR
-                        </h3>
-                    </div>
-                    <div class="col-lg-4 col-md-5 col-sm-12 col-xs-12">
-                        <ul class="links-right">
-                            <li><a href="#" class="hvr-forward">Conheça nossos serviços <i class="fa fa-caret-right"></i></a></li>
-                            <li><a href="#" class="hvr-forward">Faça um orçamento online <i class="fa fa-caret-right"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="container" style="padding-bottom:30px">
-                <div class="row row-banner1">
-                    <div class="col-md-10 col-sm-10 col-sm-offset-2 col-md-offset-2 text-center">
-                        <a href="#" class="hvr-float-shadow botao-acao-banner">SOLICITE UMA CONSULTA</a>
-                    </div>
-                </div>
-            </div>
-            <div class="clearfix"></div>
-            <section class="section-home">
-                <div class="container-fluid">
-                    <div class="row-fluid">
-                        <img src="assets/img/servicos-quem-somos.jpg" alt="" title="" class="img-responsive pull-right">
-                    </div>
-                </div>
-            </section>
-            <div class="clearfix"></div>
-            
-          <?php require 'footer.php'; ?>
         </div>
-        <div class="clearfix"></div>
+    </div>
+    <div class="container">
+        <div class="row row-banner">
+            <div class="col-lg-7 col-lg-offset-1 col-md-5 col-md-offset-2 col-sm-9 col-sm-offset-3 col-xs-12">
+                <h3 class="principal-title">
+                    O GRUPO CIATOS PRESTA TODOS OS SERVIÇOS CONTÁBEIS QUE SUA EMPRESA POSSA PRECISAR
+                </h3>
+            </div>
+            <div class="col-lg-4 col-md-5 col-sm-12 col-xs-12">
+                <ul class="links-right">
+                    <li><a href="#" class="hvr-forward">Conheça nossos serviços <i class="fa fa-caret-right"></i></a></li>
+                    <li><a href="#" class="hvr-forward">Faça um orçamento online <i class="fa fa-caret-right"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container" style="padding-bottom:30px">
+        <div class="row row-banner1">
+            <div class="col-md-10 col-sm-10 col-sm-offset-2 col-md-offset-2 text-center">
+                <a href="#" class="hvr-float-shadow botao-acao-banner">SOLICITE UMA CONSULTA</a>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    <section class="section-home">
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <img src="assets/img/servicos-quem-somos.jpg" alt="" title="" class="img-responsive pull-right">
+            </div>
+        </div>
+    </section>
+    <div class="clearfix"></div>
+    <section class="section-home ptb-home">
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="col-lg-12 ">
+                    <div class="align-middle">
+                        <img src="assets/img/sections/conheca-mais.png" alt="" title="" class="img-responsive">
+                    </div>
+                    <div class="clearfix"></div>
+                    <h2 class="heading-about-inverse">Quer conhecer mais sobre a empresa?
+                        <hr>
+                    </h2>
+                    <p>Equipe treinada com atuação diversificada e as melhores soluções contábeis para a sua empresa!</p>
+                    <div class="align-middle">
 
-    </body>
+                        <a href="#" class="stroke-red-button hvr-float-shadow">Leia Mais Aqui!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="clearfix"></div>
+    <section class="section-home ptb-home" style="background: url(assets/img/bg_porque_contratar.png) top center no-repeat; background-size: cover">
+        <div class="container-fluid">
+            <div class="row-fluid text-center">
+                <h2 class="heading-about white">Por que contratar os serviços<br>da Ciatos Contabilidade?</h2>
+            </div>
+            <div class="space-50"></div>
+            <div class="row-fluid text-center whys">
+                <div class="col-md-6 col-xs-12">
+                    <img src="assets/img/porque-contratar/1.png" alt="" title="" class="img-responsive img-center">
+                    <p>Preocupação com o cliente</p>
+                </div>
+                <div class="col-md-6 col-xs-12">
+                    <img src="assets/img/porque-contratar/2.png" alt="" title="" class="img-responsive img-center">
+                    <p>Atendimento Personalizado</p>
+                </div>
+                <div class="col-md-6 col-xs-12">
+                    <img src="assets/img/porque-contratar/3.png" alt="" title="" class="img-responsive img-center">
+                    <p>Serviços de Qualidade</p>
+                </div>
+                <div class="col-md-6 col-xs-12">
+                    <img src="assets/img/porque-contratar/4.png" alt="" title="" class="img-responsive img-center">
+                    <p>Preço Baixo</p>
+                </div>
+                <div class="col-md-6 col-xs-12">
+                    <img src="assets/img/porque-contratar/5.png" alt="" title="" class="img-responsive img-center">
+                    <p>Gestão Financeira Contábil</p>
+                </div>
+                <div class="col-md-6 col-xs-12">
+                    <img src="assets/img/porque-contratar/6.png" alt="" title="" class="img-responsive img-center">
+                    <p>Contabilidade On-line</p>
+                </div>
+                <div class="col-md-6 col-xs-12 col-md-offset-3">
+                    <img src="assets/img/porque-contratar/6.png" alt="" title="" class="img-responsive img-center">
+                    <p>Equipe Multidisciplinar</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="clearfix"></div>
+    <section class="section-home ptb-home">
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="col-lg-12 ">
+                    <div class="align-middle">
+                        <img src="assets/img/sections/departamento-pessoal.png" alt="" title="" class="img-responsive">
+                    </div>
+                    <div class="clearfix"></div>
+                    <h2 class="heading-about-inverse">DEPARTAMENTO PESSOAL
+                        <hr>
+                    </h2>
+                    <p>A Ciatos Contabilidade terceiriza todo departamento pessoal da sua empresa<br> e você paga um pequeno valor por empregado.</p>
+                    <div class="align-middle">
+
+                        <a href="#" class="stroke-red-button hvr-float-shadow">Clique aqui e saiba mais!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="clearfix"></div>
+    <section class="section-home ptb-home red-bg">
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="col-lg-12 text-center">
+                    <div class="clearfix"></div>
+                    <h2 class="heading-about white"><i>JUNTE-SE A NÓS</i></h2>
+                    <div class="align-middle">
+                        <ul class="list-unstyled list-inline">
+                            <li><a href="#" class="hvr-float-shadow"><img src="assets/img/junte-se/1.png" alt="" title="" class="img-responsive"></a></li>
+                            <li><a href="#" class="hvr-float-shadow"><img src="assets/img/junte-se/2.png" alt="" title="" class="img-responsive"></a></li>
+                            <li><a href="#" class="hvr-float-shadow"><img src="assets/img/junte-se/3.png" alt="" title="" class="img-responsive"></a></li>
+                        </ul>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="clearfix"></div>
+    <section class="section-home ptb-home">
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="col-lg-12 ">
+                    <div class="align-middle">
+                        <img src="assets/img/sections/fale-consultor.png" alt="" title="" class="img-responsive">
+                    </div>
+                    <div class="clearfix"></div>
+                    <h2 class="heading-about-inverse">fale com um consultor
+                        <hr>
+                    </h2>
+                    <p>Fale com um de nossos consultores e esclareça suas dúvidas. Se voce tem alguma questão, deixe-nos saber.</p>
+                    <div class="align-middle">
+
+                        <a href="#" class="stroke-red-button hvr-float-shadow">Clique aqui!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="clearfix"></div>
+    <section class="section-home ptb-home gray-bg">
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="col-lg-12 ">
+                    <div class="align-middle">
+                        <img src="assets/img/sections/orcamento-online.png" alt="" title="" class="img-responsive">
+                    </div>
+                    <div class="clearfix"></div>
+                    <h2 class="heading-about-inverse">FAÇA UM ORÇAMENTO ON-LINE AGORA!
+                        <hr>
+                    </h2>
+                    <p>E saiba no ato quanto vai custar para sua empresa os serviços de Contabilidade da Ciatos.</p>
+                    <div class="align-middle">
+
+                        <a href="#" class="stroke-red-button hvr-float-shadow">Vamos começar!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="clearfix"></div>
+
+    <?php require 'footer.php'; ?>
+</div>
+<div class="clearfix"></div>
+
+</body>
 
 </html>
